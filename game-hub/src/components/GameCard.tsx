62c14c604a6108c681487a46547f17eaafad89a2
Tobias Johansson <tobiasp91@hotmail.com>
2024-01-03 14:32:15 +0100

    Building game cards

---
+++ game-hub/src/components/GameCard.tsx
@@ -0,0 +1,18 @@
+import { Card, CardBody, CardHeader, Heading, Image } from "@chakra-ui/react";
+import { Game } from "../hooks/useGames";
+
+interface Props {
+  game: Game;
+}
+const GameCard = ({ game }: Props) => {
+  return (
+    <Card overflow="hidden" borderRadius={10}>
+      <Image src={game.background_image} />
+      <CardBody>
+        <Heading fontSize="2xl">{game.name}</Heading>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GameCard;
